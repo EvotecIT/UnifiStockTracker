062d00c5babb1b4c2979e42cc59a1e9a5a5fb293
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-09-21 20:41:58 +0200

    Update examples

--- Examples/GetUnifiStock.ps1
+++ Examples/GetUnifiStock.ps1
@@ -2,3 +2,4 @@
 
 Get-UnifiStock -Store USA -Collection Protect, ProtectAccessories, ProtectNVR | Sort-Object -Property Name | Format-Table
 Get-UnifiStock -Store Europe -Collection Protect, NetworkWifi | Sort-Object -Property Name | Format-Table
+Get-UnifiStock -Store Europe | Sort-Object -Property Name | Format-Table
--- Examples/WaitUnifiStock.ps1
+++ Examples/WaitUnifiStock.ps1
@@ -1,3 +1,4 @@
 ﻿Import-Module .\UnifiStockTracker.psd1 -Force
 
-Wait-UnifiStock -ProductName 'UniFi6 Mesh', 'G4 Doorbell Pro', 'Camera G4 Pro' -Seconds 60
+Wait-UnifiStock -ProductName 'UniFi6 Mesh', 'G4 Doorbell Pro', 'Camera G4 Pro', 'Test' -Seconds 60
+Wait-UnifiStock -ProductSKU 'UDR-EU' -ProductName 'Switch Flex XG' -Seconds 60
