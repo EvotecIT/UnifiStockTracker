2c3cbd06d71a358ecfc6638f476fc437b0ef7da1
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-04-15 20:09:21 +0200

    Example update

--- Examples/WaitUnifiStock.ps1
+++ Examples/WaitUnifiStock.ps1
@@ -2,3 +2,4 @@
 
 Wait-UnifiStock -ProductName 'UniFi6 Mesh', 'G4 Doorbell Pro', 'Camera G4 Pro', 'Test' -Seconds 60
 Wait-UnifiStock -ProductSKU 'UDR-EU' -ProductName 'Switch Flex XG' -Seconds 60
+Wait-UnifiStock -ProductName 'Access Point AC Lite' -Seconds 60
