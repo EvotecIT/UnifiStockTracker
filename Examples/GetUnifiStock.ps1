fc786d28d527bc8c4e83cb84062eb46f9f0efd12
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-04-17 16:20:45 +0200

    Update example

--- Examples/GetUnifiStock.ps1
+++ Examples/GetUnifiStock.ps1
@@ -1,5 +1,5 @@
 ﻿Import-Module .\UnifiStockTracker.psd1 -Force
 
-Get-UnifiStock -Store USA -Collection Protect, ProtectAccessories, ProtectNVR | Sort-Object -Property Name | Format-Table
-Get-UnifiStock -Store Europe -Collection Protect, NetworkWifi | Sort-Object -Property Name | Format-Table
-Get-UnifiStock -Store Europe | Sort-Object -Property Name | Format-Table
+Get-UnifiStock -Store USA -Collection Protect, ProtectAccessories, ProtectNVR -Verbose | Sort-Object -Property Name | Format-Table
+Get-UnifiStock -Store Europe -Collection Protect, NetworkWifi -Verbose | Sort-Object -Property Name | Format-Table
+Get-UnifiStock -Store Europe -Verbose | Sort-Object -Property Name | Format-Table
