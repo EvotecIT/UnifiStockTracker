298e0c695a618dd9d0b71328b807c094e5d70fd6
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-04-17 17:22:42 +0200

    Update examples

--- Examples/GetUnifiStock.ps1
+++ Examples/GetUnifiStock.ps1
@@ -1,5 +1,11 @@
 ﻿Import-Module .\UnifiStockTracker.psd1 -Force
 
-Get-UnifiStock -Store USA -Collection Protect, ProtectAccessories, ProtectNVR -Verbose | Sort-Object -Property Name | Format-Table
-Get-UnifiStock -Store Europe -Collection Protect, NetworkWifi -Verbose | Sort-Object -Property Name | Format-Table
-Get-UnifiStock -Store Europe -Verbose | Sort-Object -Property Name | Format-Table
+#Get-UnifiStock -Store USA -Collection Protect, ProtectAccessories, ProtectNVR | Sort-Object -Property Name | Format-Table
+#Get-UnifiStock -Store Europe -Collection Protect, NetworkWifi | Sort-Object -Property Name | Format-Table
+#Get-UnifiStock -Store Europe | Sort-Object -Property Name | Format-Table
+
+#Get-UnifiStock -Store USA -Collection UnifiAccessories | Sort-Object -Property Name | Format-Table
+#Get-UnifiStock -Store Brazil -Collection UnifiAccessories | Sort-Object -Property Name | Format-Table
+#Get-UnifiStock -Store Europe -Collection UnifiAccessories | Sort-Object -Property Name | Format-Table
+#Get-UnifiStock -Store Europe -Collection EarlyAccess | Sort-Object -Property Name | Format-Table
+Get-UnifiStock -Store USA -Collection EarlyAccessConnect,EarlyAccessDoorAccess | Sort-Object -Property Name | Format-Table
