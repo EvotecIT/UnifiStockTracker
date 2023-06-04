5928aea8c2b7e1b53a75596ae72b64d06a6129ec
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-06-04 20:38:41 +0200

    Update example to new changes

---
+++ Examples/GetUnifiStockLegacy.ps1
@@ -0,0 +1,11 @@
+﻿Import-Module .\UnifiStockTracker.psd1 -Force
+
+# following examples will only work for non-eu, non-us shops that are not yet converted to the new shop
+Get-UnifiStockLegacy -Store Brazil -Collection Protect, ProtectAccessories, ProtectNVR | Sort-Object -Property Name | Format-Table
+Get-UnifiStockLegacy -Store Brazil -Collection Protect, NetworkWifi | Sort-Object -Property Name | Format-Table
+Get-UnifiStockLegacy -Store Brazil | Sort-Object -Property Name | Format-Table
+Get-UnifiStockLegacy -Store Brazil -Collection UnifiAccessories | Sort-Object -Property Name | Format-Table
+Get-UnifiStockLegacy -Store Brazil -Collection UnifiAccessories | Sort-Object -Property Name | Format-Table
+Get-UnifiStockLegacy -Store Brazil -Collection UnifiAccessories | Sort-Object -Property Name | Format-Table
+Get-UnifiStockLegacy -Store Brazil -Collection EarlyAccess | Sort-Object -Property Name | Format-Table
+Get-UnifiStockLegacy -Verbose -Store Brazil -Collection UnifiPhoneSystem, EarlyAccessConnect, EarlyAccessDoorAccess | Sort-Object -Property Name | Format-Table
--- Examples/WaitUnifiStock.ps1
+++
@@ -1,5 +0,0 @@
-﻿Import-Module .\UnifiStockTracker.psd1 -Force
-
-Wait-UnifiStock -ProductName 'UniFi6 Mesh', 'G4 Doorbell Pro', 'Camera G4 Pro', 'Test' -Seconds 60
-Wait-UnifiStock -ProductSKU 'UDR-EU' -ProductName 'Switch Flex XG' -Seconds 60
-Wait-UnifiStock -ProductName 'Access Point AC Lite' -Seconds 60
